dd7ff54359352bfcc5d2abf8513bc80c2cda4d18
stefan-ysh <58702199+stefan-ysh@users.noreply.github.com>
2022-01-04 19:48:51 +0800

    Update TestButton.vue

--- test/TestButton.vue
+++ test/TestButton.vue
@@ -2,19 +2,43 @@
   <div class="app">
     <div class="row">
       <h4>Original</h4>
-     <y-input></y-input>
+      <y-button>Button</y-button>
+      <y-button type="primary">Button</y-button>
+      <y-button type="success">Button</y-button>
+      <y-button type="info">Button</y-button>
+      <y-button type="warning">Button</y-button>
     </div>
     <div class="row">
       <h4>Plain</h4>
+      <y-button plain>Button</y-button>
+      <y-button plain type="primary">Button</y-button>
+      <y-button plain type="success">Button</y-button>
+      <y-button plain type="info">Button</y-button>
+      <y-button plain type="warning">Button</y-button>
     </div>
     <div class="row">
       <h4>Round</h4>
+      <y-button round plain>Button</y-button>
+      <y-button round plain type="primary">Button</y-button>
+      <y-button round plain type="success">Button</y-button>
+      <y-button round plain type="info">Button</y-button>
+      <y-button round plain type="warning">Button</y-button>
     </div>
     <div class="row">
       <h4>Circle</h4>
+      <y-button circle>OK</y-button>
+      <y-button circle type="primary">OK</y-button>
+      <y-button circle type="success">OK</y-button>
+      <y-button circle type="info">OK</y-button>
+      <y-button circle type="warning">OK</y-button>
     </div>
     <div class="row">
       <h4>Font</h4>
+      <y-button plain>字体</y-button>
+      <y-button icon="y-icon-check" circle plain type="primary"></y-button>
+      <y-button icon="y-icon-message" circle plain type="success"></y-button>
+      <y-button icon="y-icon-search" circle plain type="info"></y-button>
+      <y-button icon="y-icon-star-off" plain type="warning">字体图标</y-button>
     </div>
   </div>
 </template>
